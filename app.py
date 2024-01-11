2dce3f4d53e8316bae96685cb296751d227b8c3e
Nelson Muriithi <nelsonmuriithi65@gmail.com>
2024-01-11 09:53:12 +0300

    setup migrations

--- app.py
+++ app.py
@@ -1,7 +1,16 @@
 from flask import Flask
+from flask_migrate import Migrate
+from models import db
 
 app = Flask(__name__)
+# configure db URI
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
 
+# link migrations
+migrations = Migrate(app, db)
+
+# init our db
+db.init_app(app)
 
 @app.route('/')
 def index():
