ad5571fe7b27c008fccddbee6e20fed313fb6ca7
Nelson Muriithi <nelsonmuriithi65@gmail.com>
2024-01-12 09:39:15 +0300

    finish up on location resource

--- resources/location.py
+++ resources/location.py
@@ -1,18 +1,42 @@
-from flask_restful import Resource
-from models import LocationModel
+from flask_restful import Resource, fields, marshal_with, reqparse
+from models import LocationModel, db
+
+resource_fields = {
+    'id': fields.Integer,
+    'name': fields.String,
+    'created_at': fields.DateTime,
+    'updated_at': fields.DateTime
+}
 
 class Location(Resource):
+    parser = reqparse.RequestParser()
+    parser.add_argument('name', required=True, help="Location name is required")
+
     # get a single location
-    def get(self, id):
-        location = LocationModel.query.filter_by(id = id).first()
-        print(location.json())
-        # print(f"Retrieving {id}")
-        return "single location"
+    @marshal_with(resource_fields)
+    def get(self, id=None):
+        # if the id is present we get a single location
+        # else we get all locations
+        if id:
+            location = LocationModel.query.filter_by(id = id).first()
 
-    def put(self, id):
-        print(f"Updating {id}")
-        return "Location update"
+            return location
+        else:
+            locations = LocationModel.query.all();
 
-    def delete(self, id):
-        print(f"Deleting #{id}")
-        return
+            return locations
+
+    def post(self):
+        data = Location.parser.parse_args()
+        # unpacks a dict add passes it as key-value pairs
+        # {"title": "Ngong"} -> title: "Ngong"
+        location = LocationModel(**data)
+
+        try:
+            db.session.add(location)
+            db.session.commit()
+
+            return {"message": "Location created successfully"}
+        except:
+            return {"message": "Unable to save location"}
+
